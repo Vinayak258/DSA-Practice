e92fbf78e69de284fef2dd52a97ebcd0cbfa5bb0
Vinayak258 <ojhavinayak147@gmail.com>
2025-11-17 21:58:47 +0530

    Day 37

---
+++ Array/setMatrixZero.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+
+        int col0 = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    if (j != 0)
+                    {
+                        matrix[0][j] = 0;
+                    }
+                    else
+                    {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}};
+
+    Solution sol;
+    sol.setZeroes(matrix);
+
+    // Print result
+    for (const auto &row : matrix)
+    {
+        for (int val : row)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
