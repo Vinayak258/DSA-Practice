2453120965839ee5f68cb0694f5521665cf24e14
Vinayak258 <ojhavinayak147@gmail.com>
2026-06-10 21:46:14 +0530

    Day 21

---
+++ Recursion/binarySearchableCount.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void solve(vector<int> &arr,
+               int l,
+               int r,
+               int mn,
+               int mx,
+               int &ans)
+    {
+
+        if (l > r)
+            return;
+
+        int mid = (l + r) / 2;
+
+        // Check whether current element is binary searchable
+        if (arr[mid] > mn && arr[mid] < mx)
+        {
+            ans++;
+        }
+
+        // Left subtree
+        solve(arr,
+              l,
+              mid - 1,
+              mn,
+              min(mx, arr[mid]),
+              ans);
+
+        // Right subtree
+        solve(arr,
+              mid + 1,
+              r,
+              max(mn, arr[mid]),
+              mx,
+              ans);
+    }
+
+    int binarySearchable(vector<int> &arr)
+    {
+
+        int ans = 0;
+
+        solve(arr,
+              0,
+              arr.size() - 1,
+              INT_MIN,
+              INT_MAX,
+              ans);
+
+        return ans;
+    }
+};
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    Solution obj;
+
+    cout << obj.binarySearchable(arr) << endl;
+
+    return 0;
+}
