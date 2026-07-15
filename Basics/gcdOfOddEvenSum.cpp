d3a1ad9b5728979dac3e9be67becfc61c165ae4e
Vinayak258 <ojhavinayak147@gmail.com>
2026-07-15 22:00:21 +0530

    Day 243

---
+++ Basics/gcdOfOddEvenSum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int gcdOfOddEvenSums(int n)
+{
+
+    // int oddSum = n*n;
+    // int evenSum = n*(n+1);
+
+    return n;
+}
+int main()
+{
+
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    cout << gcdOfOddEvenSums(n) << endl;
+    return 0;
+}
